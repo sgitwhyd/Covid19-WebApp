6fd58eb3a0c2807091a02ae5cb60133707237b66
Sigit Wahyudi <sigitwahyu.mutu@gmail.com>
2021-02-24 20:00:33 +0700

    add: format number

--- src/script/view/data.js
+++ src/script/view/data.js
@@ -1,17 +1,25 @@
-    const reqApi = async() => {
-        const baseUrl = "https://covid19.mathdro.id/api/countries/IDN"
-        try {
-            const response = await fetch(baseUrl)
-            const responseJson = await response.json();
-            viewData(responseJson);
-        }catch(error){
-            console.log(error)
-        }
-    }
+const format = require('format-number')
 
-    const viewData = (responseJson) => {
-        const covidContainer = document.querySelector('#covid-container');
-        covidContainer.innerHTML = `<div class="container">
+const reqApi = async () => {
+    const baseUrl = "https://covid19.mathdro.id/api/countries/IDN"
+    try {
+        const response = await fetch(baseUrl)
+        const responseJson = await response.json();
+        viewData(responseJson);
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+
+const viewData = (responseJson) => {
+    const formatNumber = number => {
+        var formattedNumber = format({ suffix: " Orang" })(number);
+        return formattedNumber
+    }
+    const covidContainer = document.querySelector('#covid-container');
+    covidContainer.innerHTML = `<div class="container">
         <h2 class="text-center mt-5 text-heading">Informasi Kasus Covid-19 di Indonesia</h2>
     <div class="row mt-4">
         <div class="col-md-4 col-sm-12">
@@ -21,7 +29,9 @@
                 </div>
                 <div class="card-body">
                     <p class="card-text text-center">
-                        <h4 class="text-center">${responseJson.confirmed.value} Orang</h4>
+                        <h4 class="text-center">
+${formatNumber(responseJson.confirmed.value)}
+            </h4>
                     </p>
                 </div>
             </div>
@@ -31,7 +41,7 @@
                   <div class="card-header text-center"><h2>Sembuh</h2></div>
                   <div class="card-body">
                       <p class="card-text">
-                      <h4 class="text-center">${responseJson.recovered.value} Orang</h4> 
+                      <h4 class="text-center">${formatNumber(responseJson.recovered.value)}</h4> 
                       </p>
                   </div>
               </div>
@@ -44,7 +54,7 @@
                         </div>
                     <div class="card-body">
                         <p class="card-text">
-                            <h4 class="text-center">${responseJson.deaths.value} Orang</h4>
+                            <h4 class="text-center">${formatNumber(responseJson.deaths.value)}</h4>
                         </p>
                     </div>
                 </div>
@@ -52,7 +62,7 @@
             </div>
             <p id="updateDate" class="mt-3"> Terakhir di Update pada ${responseJson.lastUpdate}</p>
         </div>;`
-    }
+}
 
-    export default reqApi;
+export default reqApi;
 
